45030df3f3b7d5a8eb7a3b9f2a62f12027a13082
hamza <144230796+earthbendbeast@users.noreply.github.com>
2024-10-25 10:34:54 +0200

    added header and footer

---
+++ Footer.php
@@ -0,0 +1,27 @@
+
+<body>
+    <footer>
+        <section class="footer-grid">
+            <article class="footer-item">
+                <h1>Gegevens</h1>
+                <p>6035005@mborijnland.nl</p>
+                <p>6038071@mborijnland.nl</p>
+                <p>+31-6123345</p>
+                <p>+31-6543211</p>
+            </article>
+
+            <article class="footer-item">
+                <h1>Openingstijden</h1>
+                <p>dinsdag t/m vrijdag</p>
+                <p>11.30-21.00u</p>
+                <p>zaterdag en zondag</p>
+                <p>16.00-21.00u</p>
+            </article>
+
+            <article class="footer-item">
+                <a class="Privacy" href="Privacy.php">Privacy</a>
+            </article>
+        </section>
+    </footer>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="">
+  <meta name="Hamzapo" content="GoldenDragon">
+  <meta name="Golden,Dragon,Chinese" content="">
+  <title>Chinese restaurant</title>
+  <link rel="stylesheet" href="css/style.css" >
+  <script src="yummy2.js"></script>
+</head>
+<body>
+<header>
+  <section>
+    <img src="img/GoldenDragon.png" alt="Your Logo">
+  </section>
+  <article class="nav">
+    <nav>
+      <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="yummy3.php">Menukaart</a></li>
+        <li><a href="Reserveer.php">Reserveer</a></li>
+        <li><a href="about.php">About us</a></li>
+        <li><a href="gallery.php">Gallery</a></li>
+        <li><a href="Vacatures.php">Vacatures</a></li>
+        <li><a href="Dranken.php">Dranken</a></li>
+      </ul>
+    </nav>
+  </article>
+
+  <section class="hamburger-menu" onclick="toggleMenu()">
+    <article class="hamburger"></article>
+    <article class="hamburger"></article>
+
+  </section>
+</header>
+</body>
