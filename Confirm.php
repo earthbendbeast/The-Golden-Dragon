8c4bd6054dbb6219e9c1e3d9e13e19790cccf54c
hamza <144230796+earthbendbeast@users.noreply.github.com>
2024-10-25 09:50:54 +0200

    Update Confirm.php

--- Confirm.php
+++ Confirm.php
@@ -2,15 +2,12 @@
 <?php
 include "inc/header.php";
 ?>
-
     <section class="main1">
         <article class="reservering-voltooid">
             <h2>Reservering Voltooid</h2>
             <p>Bedankt voor je reservering! We kijken ernaar uit je te verwelkomen.</p>
         </article>
     </section>
-
-
 <?php
 include "inc/Footer.php";
 ?>
